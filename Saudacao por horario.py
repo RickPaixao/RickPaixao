b91a4408645af945fc7d068e0c78e7ee89433a28
Henrique Mota Paixao <110409895+RickPaixao@users.noreply.github.com>
2024-06-06 18:10:49 -0300

    Atualizar o Saudacao por horario.py

--- Saudacao por horario.py
+++ Saudacao por horario.py
@@ -1,18 +1,23 @@
+import datetime
+
+hora = datetime.datetime.now()
+hora = str(hora)
+hr = hora[11:13]
+
 nome = input('Ola, digite seu nome: ')
 print()
-print(f'Ola {nome}, tudo bem?...')
-h1 = input(f'{nome}, que hora são?: ')
+print(f'{nome}, Agora são {hora[11:16]}')
 
-if h1.isdigit():
-    h1 = int(h1)
-    if h1 < 0 or h1 > 23:
+if hr.isdigit():
+    hr = int(hr)
+    if hr < 0 or hr > 23:
         print(f'{nome}, Digite um horario entre 0. Game e 23.')
     else:
-        if    h1 <= 11:
+        if    hr <= 11:
             print(f'Bom Dia {nome}, ainda está cedo né')
-        elif h1 <= 17:
+        elif hr <= 17:
             print(f'Boa Tarde {nome}, nossa já está de tarde')
         else:
            print(f'Boa Noite {nome}, Escureceu rápido hoje né?')
 else:
-    print(f'{nome}, acho que o que você digitou "{h1}" não é um horário valido.')
+    print(f'{nome}, acho que o que você digitou "{hr}" não é um horário valido.')
